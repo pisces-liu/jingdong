61375590fffa555776ac0e4cf24bd9cc344ac157
piscesliu <piscesliu66@qq.com>
2022-05-10 14:31:53 +0800

    完成首页部分

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,217 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe6a7;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xe7c4;</span>
+    </div>
+    <div class="search">
+      <span class="iconfont">&#xe601;</span>
+      <span class="search__text">山姆会员商品优惠商品</span>
+    </div>
+    <div class="banner">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        class="banner__img"
+        alt=""
+      />
+    </div>
+    <div class="icons">
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          alt=""
+          class="icons__item__img"
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+    </div>
+    <div class="gap"></div>
+    <div class="nearby">
+      <h3 class="nearby__title">附近店铺</h3>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__tags">
+            <div class="nearby__content__title">沃尔玛</div>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+            <span class="nearby__content__tag">月售一万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="docker">
     <div class="docker__item docker__item--active">
       <div class="iconfont">&#xe92d;</div>
@@ -20,6 +233,129 @@
 </template>
 
 <style lang="scss">
+@import "./style/viriables.scss";
+@import "./style/mixins.scss";
+.wrapper {
+  position: absolute;
+  overflow-y: auto;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem; // 与底部的 docker 保持距离
+  right: 0;
+  padding: 0 .18rem .1rem .18rem;
+}
+.position {
+  position: relative;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  color: $content-fontcolor;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: 0.01rem;
+    font-size: 0.2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+    font-size: 0.2rem;
+  }
+}
+.search {
+  margin-bottom: 0.12rem;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  .iconfont {
+    display: inline-block;
+    padding: 0 0.08rem 0 0.16rem;
+    font-size: 0.16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: 0.14rem;
+  }
+}
+
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%; // 撑开图片容器的高度，25.4% 是图片的高宽比。
+  &__img {
+    width: 100%;
+  }
+}
+
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 0.16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: 0.06rem 0 0.16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+
+.gap {
+  margin: 0 -0.18rem;
+  height: 0.1rem;
+  background: $content-bgColor;
+}
+.nearby {
+  &__title {
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+  &__item {
+    display: flex;
+    padding-top: 0.12rem;
+    &__img {
+      margin-right: 0.16rem;
+      width: 0.56rem;
+      height: 0.56rem;
+    }
+  }
+  &__content {
+    flex: 1;
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $content-bgColor;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__highlight {
+      margin: 0.08rem 0 0 0;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: #e93b3b;
+    }
+  }
+}
 .docker {
   display: flex;
   position: absolute;
@@ -30,6 +366,7 @@
   width: 100%;
   height: 0.49rem;
   border-top: 1px solid #f1f1f1;
+  color: $content-fontcolor;
 
   &__item {
     flex: 1;
@@ -47,6 +384,7 @@
     font-size: 0.2rem;
     text-align: center;
     transform: scale(0.5, 0.5);
+    transform-origin: center top;
   }
 }
 </style>
